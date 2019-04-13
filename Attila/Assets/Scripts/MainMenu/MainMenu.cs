7943a063e4b4f7e3bec3243304a0fbdc83ed75c5
cocodroloapps <cocodroloapps@gmail.com>
2019-04-13 08:55:54 +0200

    Main menu + Stages Menu Base

---
+++ Attila/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GlobalInfo.sessionsCount++;
+        StartCoroutine(SaveSessionsConfig());
+    }
+
+    IEnumerator SaveSessionsConfig()
+    {
+        yield return new WaitForSeconds(0.5f);
+        PlayerInfo loadedData = DataSaver.loadData<PlayerInfo>(GlobalInfo.configFile, "txt");        
+        loadedData.sessionsCount = GlobalInfo.sessionsCount;
+        DataSaver.saveData(loadedData, GlobalInfo.configFile, "txt");        
+    }
+
+    public void PlayGame()
+    {
+        if (GlobalInfo.playFirstTime == true)
+        {
+            // Save PLAYDATEFIRSTTIME info to CONFIG
+            PlayerInfo loadedData = DataSaver.loadData<PlayerInfo>(GlobalInfo.configFile, "txt");
+            loadedData.playDateFirstTime = DateTime.Now.ToBinary().ToString();
+            DataSaver.saveData(loadedData, GlobalInfo.configFile, "txt");
+            GlobalInfo.playFirstTime = false;
+        }
+        SceneManager.LoadScene("StageSelector");
+    }
+}
---
+++ Attila/Assets/Scripts/Stages/StageManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ToGameScene()
+    {
+        SceneManager.LoadScene("Attila");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
